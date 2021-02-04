2573ff8ea9651d03a160451d400db7aef4f9d480
camagar <mauriciogrestrepo@gmail.com>
2021-02-04 01:59:45 +0000

    quick sort fixed

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,71 +1,89 @@
 #include "sort.h"
 
 /**
- * quick_sort -  function that sorts an array of integers in ascending
- * order using the Quick sort algorithm
- * @array: pointer to the array
- * @size: the size
+ * swap - swap items
+ * @a: item 1
+ * @b: item 2
+ *
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * partition - search pivot and swap items
+ * @array: The array to be printed
+ * @lo: left position init
+ * @hi: right position
+ * @size: Number of elements in @array
+ *
+ * Return: pivot number
+ */
+int partition(int *array, ssize_t lo, ssize_t hi, size_t size)
+{
+	int pivot = array[hi];
+	ssize_t j = lo, i;
+
+	for (i = lo; i < hi; i++)
+	{
+		if (array[i] < pivot)
+		{
+			if (j != i)
+			{
+				swap(&array[j], &array[i]);
+				print_array(array, size);
+			}
+
+			j++;
+		}
+	}
+
+	if (hi != j && array[hi] != array[j])
+	{
+		swap(&array[hi], &array[j]);
+		print_array(array, size);
+	}
+
+	return (j);
+}
+
+/**
+ * quicksort - quick sort function with more parameters
+ * @array: The array to be printed
+ * @lo: left position init
+ * @hi: right position
+ * @size: Number of elements in @array
+ *
+ * Return: void
+ */
+void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
+{
+	if (lo < hi)
+	{
+		int pivot = partition(array, lo, hi, size);
+
+		quicksort(array, lo, pivot - 1, size);
+		quicksort(array, pivot + 1, hi, size);
+	}
+}
+
+/**
+ * quick_sort - quick sort function
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ *
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL || size < 2)
+		return;
+
 	quicksort(array, 0, size - 1, size);
-}
-
-/**
- * quicksort - recursive sort function
- * @array: pointer to the array
- * @size: array size
- * @hi: the hi
- * @lo: the hish limit
- */
-void quicksort(int *array, int hi, int lo, size_t size)
-{
-	int piv;
-
-	if (hi < lo)
-	{
-		piv = partition(array, hi, lo, size);
-		quicksort(array, hi, piv - 1, size);
-		quicksort(array, piv + 1, lo, size);
-	}
-}
-
-/**
- * partition - function tha creates a partition and order the array
- * @array: the array
- * @size: the size
- * @hi: the hi limit
- * @lo: the hish limit
- * Return: return the pivot piv
- */
-size_t partition(int *array, int hi, int lo, size_t size)
-{
-	int piv, i, j, k;
-
-	piv = array[lo];
-
-	i = hi - 1;
-	for (j = hi; j < lo; j++)
-	{
-		if (array[j] <= piv)
-		{
-			i++;
-			if (i != j)
-			{
-				k = array[i];
-				array[i] = array[j];
-				array[j] = k;
-				print_array(array, size);
-			}
-		}
-	}
-	i++;
-	if (i != j)
-	{
-		k = array[i];
-		array[i] = array[lo];
-		array[lo] = k;
-		print_array(array, size);
-	}
-	return (i);
 }
